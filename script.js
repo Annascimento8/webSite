967838fcf688ed7f67411463a2725509c438a31f
Annascimento8 <anascimentoreis8@gmail.com>
2026-03-24 22:44:24 -0300

    Atualizando layout do site inicial

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. Buscando e guardando os elementos em variáveis (Árvore DOM)
+// Buscando e guardando os elementos em variáveis (Árvore DOM)
 const btnIncrementar = document.getElementById('btnIncrementar');
 const btnDecrementar = document.getElementById('btnDecrementar');
 const contadorDisplay = document.getElementById('contadorDisplay');
@@ -16,14 +16,14 @@ const btnReset = document.getElementById('btnReset');
 // Variável de controle do contador
 let totalCliques = 0;
 
-// --- LÓGICA DO CONTADOR ---
+//  LÓGICA DO CONTADOR 
 btnIncrementar.addEventListener('click', function() {
     totalCliques++;
     contadorDisplay.innerText = totalCliques;
 });
 
 btnDecrementar.addEventListener('click', function() {
-    // Estrutura de controle para não permitir valores negativos
+    
     if (totalCliques > 0) {
         totalCliques--;
         contadorDisplay.innerText = totalCliques;
@@ -32,31 +32,31 @@ btnDecrementar.addEventListener('click', function() {
     }
 });
 
-// --- LÓGICA DE TEXTO E CARACTERES ---
+// LÓGICA DE TEXTO E CARACTERES 
 inputTexto.addEventListener('input', function() {
-    // Contador de caracteres em tempo real (sem espaços)
+    
     let textoSemEspaco = inputTexto.value.replace(/\s/g, '');
     contadorChar.innerText = textoSemEspaco.length;
 });
 
 inputTexto.addEventListener('keydown', function(event) {
-    // Adiciona parágrafo ao pressionar Enter
+   
     if (event.key === 'Enter') {
         if (inputTexto.value.trim() !== "") {
             let novoP = document.createElement('p');
             novoP.innerText = inputTexto.value;
             containerParagrafos.appendChild(novoP);
             
-            // Limpa o campo e o contador de caracteres após inserir
+            
             inputTexto.value = "";
             contadorChar.innerText = "0";
         }
     }
 });
 
-// --- LÓGICA DE ADICIONAR ITEM (LISTAS) ---
+// LÓGICA DE ADICIONAR ITEM (LISTAS) 
 btnAddItem.addEventListener('click', function() {
-    // Cria o elemento com base na escolha do usuário (ul ou ol)
+    
     let listaPai = document.createElement(tipoLista.value);
     let li = document.createElement('li');
     
@@ -66,18 +66,17 @@ btnAddItem.addEventListener('click', function() {
     containerListas.appendChild(listaPai);
 });
 
-// --- LÓGICA DE RESET ---
+// LÓGICA DE RESET
 btnReset.onclick = function() {
-    // Zerando contador
+  
     totalCliques = 0;
     contadorDisplay.innerText = "0";
     
-    // Limpando textos
+    
     inputTexto.value = "";
     contadorChar.innerText = "0";
     containerParagrafos.innerHTML = "";
     
-    // Removendo as listas
     containerListas.innerHTML = "";
     
     console.log("Sistema resetado com sucesso.");
